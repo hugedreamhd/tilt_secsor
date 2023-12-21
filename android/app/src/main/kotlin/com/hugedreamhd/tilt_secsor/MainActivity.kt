826fc2ea814d5d3827e1cfe0923b72522460828f
Sangin Han <hugedreamhd@gmail.com>
2023-12-21 23:41:37 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/hugedreamhd/tilt_secsor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hugedreamhd.tilt_secsor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SensorApp(),
+    );
+  }
+}
+
+class SensorApp extends StatelessWidget {
+  const SensorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: centerX,
+            top: centerY,
+            child: Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
